72366cfccd749de727eed8b4086c5528c979e5c6
Szymon Skulimowski <szymskul@gmail.com>
2024-06-16 00:45:55 +0200

    Add matrix and medium classification

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,12 +1,9 @@
 import ssl
-import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import MinMaxScaler
 from ucimlrepo import fetch_ucirepo
 import Kohonen
-import MLP
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -28,10 +25,10 @@ scaler = MinMaxScaler(feature_range=(-1, 1))
 
 X = scaler.fit_transform(X)
 
-X_processed_kohonen_train = X[0:500]
-X_processed_kohonen_test = X[500:600]
-Y_processed_kohonen_train = Y[0:500]
-Y_processed_kohonen_test = Y[500:600]
+X_processed_kohonen_train = X[0:2000]
+X_processed_kohonen_test = X[2000:2700]
+Y_processed_kohonen_train = Y[0:2000]
+Y_processed_kohonen_test = Y[2000:2700]
 
 som_size = (20, 10)
 input_len = X.shape[1]
@@ -45,6 +42,29 @@ score = som.quantization_error(X_processed_kohonen_train)
 print("Quantization before")
 print(score)
 
+som.classificationOfNeurons(X_processed_kohonen_train, Y_processed_kohonen_train)
+
+predicted_before = som.testKohonen(X_processed_kohonen_test)
+
+correct = 0
+medium_level = 0
+Y_target = []
+
+for process in Y_processed_kohonen_test:
+    Y_target.append(process[0])
+for i in range(len(Y_processed_kohonen_test)):
+    if Y_target[i] == predicted_before[i]:
+        correct += 1
+    elif Y_target[i] == predicted_before[i] - 1 or Y_target[i] == predicted_before[i] + 1:
+        medium_level += 1
+
+medium_level = medium_level / len(Y_processed_kohonen_test) * 100
+
+correct = correct/len(Y_processed_kohonen_test) * 100
+print(f"Całkowity procent poprawnie rozpoznanych przypadków {correct}%")
+print(f"Calkowity procent srednio poprawnie rozpoznanych przypadkow {medium_level}%")
+print(f"Calkowity procent zle rozpoznanych przypadkow {100 - correct - medium_level}%")
+
 som.trainKohonen(X_processed_kohonen_train, numberOfEpochs)
 
 som.classificationOfNeurons(X_processed_kohonen_train, Y_processed_kohonen_train)
@@ -55,6 +75,7 @@ score = som.quantization_error(X_processed_kohonen_train)
 print("Quantization after")
 print(score)
 correct = 0
+medium_level = 0
 Y_target = []
 
 for process in Y_processed_kohonen_test:
@@ -62,66 +83,14 @@ for process in Y_processed_kohonen_test:
 for i in range(len(Y_processed_kohonen_test)):
     if Y_target[i] == predicted[i]:
         correct += 1
+    elif Y_target[i] == predicted[i] - 1 or Y_target[i] == predicted[i] + 1:
+        medium_level += 1
 
-Y_Set = []
-for process in Y_processed:
-    Y_Set.append(process[0])
+matrix = confusion_matrix(Y_target, predicted)
+print(matrix)
 
+medium_level = medium_level / len(Y_processed_kohonen_test) * 100
 correct = correct/len(Y_processed_kohonen_test) * 100
-print(f"Całkowity procent poprawnie rozpoznanych przypadków kohonen {correct}%")
-
-target_values = []
-for number in Y_Set:
-    if number == 0:
-        target_values.append([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-    elif number == 1:
-        target_values.append([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-    elif number == 2:
-        target_values.append([0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
-    elif number == 3:
-        target_values.append([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
-    elif number == 4:
-        target_values.append([0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0])
-    elif number == 5:
-        target_values.append([0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0])
-    elif number == 6:
-        target_values.append([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0])
-    elif number == 7:
-        target_values.append([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
-    elif number == 8:
-        target_values.append([0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0])
-    elif number == 9:
-        target_values.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0])
-    elif number == 10:
-        target_values.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
-
-
-target_values = np.array(target_values)
-combined_data = list(zip(X, target_values))
-train_data = combined_data[0:500]
-test_data = combined_data[500:600]
-
-network = MLP.MLP(11, [12,15,12], 11, True)
-
-train_error_list, correct_predictions_list, number_of_epoch = network.train(data_set=train_data, number_of_epochs=1000, momentum=0, learning_Rate=0.4, bias=True , test_data=test_data,shuffle=True, stop_value=None)
-plt.figure(figsize=(12, 5))
-
-plt.subplot(1, 2, 1)
-plt.plot(range(1, number_of_epoch), train_error_list, linestyle='-', color='b')
-plt.title('Błędy treningowe w zależności od liczby epok')
-plt.xlabel('Liczba epok')
-plt.ylabel('Błąd treningowy')
-plt.grid(True)
-
-plt.subplot(1, 2, 2)
-plt.plot(range(1, number_of_epoch), correct_predictions_list, linestyle='-', color='g')
-plt.title('Dokładność w zależności od liczby epok')
-plt.xlabel('Liczba epok')
-plt.ylabel('Dokładność')
-plt.ylim(0, 1)
-plt.grid(True)
-
-plt.tight_layout()
-plt.show()
-
-network.test(train_data, "test2")
+print(f"Całkowity procent poprawnie rozpoznanych przypadków {correct}%")
+print(f"Calkowity procent srednio poprawnie rozpoznanych przypadkow {medium_level}%")
+print(f"Calkowity procent zle rozpoznanych przypadkow {100 - correct - medium_level}%")
